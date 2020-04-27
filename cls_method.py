b66716b92e028710acc124a5d0342f0a81ba4569
祥 <akiraamiya@yahoo.co.jp>
2020-04-27 18:41:02 +0900

    cls_method code_practice

--- cls_method.py
+++ cls_method.py
@@ -7,28 +7,28 @@ class Foo:
 
     def instance_method(self):
         print('in instance_method')
-        print('cls ver:',self.clsvar)
-        print('instance var:',self.name)
+        print('cls ver:', self.clsvar)
+        print('instance var:', self.name)
 
     @classmethod
     def cls_method(cls):
         print('in cls_method')
-        print('cls var:',cls.clsvar)
+        print('cls var:', cls.clsvar)
 
-    @staricmethod
-    def static_method(self):
+    @staticmethod
+    def static_method():
         print('in static_method')
-        print('cls var:',Foo.clsvar)
+        print('cls var:', Foo.clsvar)
 
-f=Foo('foo')
+f = Foo('foo')
 f.instance_method()
 f.cls_method()
 Foo.cls_method()
 Foo.static_method()
 print()
-b=Foo('bar')
+b = Foo('bar')
 b.instance_method()
-print('Foo.clsvar:',b.clsvar)
+print('Foo.clsvar:', b.clsvar)
 b.clsvar += 1
-print('b.clsbar:',b.clsvar)
-print('Foo.clsbar:',Foo.clsvar)
+print('b.clsbar:', b.clsvar)
+print('Foo.clsbar:', Foo.clsvar)
